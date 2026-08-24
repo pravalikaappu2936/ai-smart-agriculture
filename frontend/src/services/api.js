c9a4f674ff3c16f4ef2dfe0d7094f6a073f09ba4
Pravalika Appu <pravalikaappu2936@gmail.com>
2026-08-24 22:31:48 +0530

    Prepare frontend for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -5,7 +5,9 @@ import axios from "axios";
 // API BASE URL
 // =========================================================
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL =
+    import.meta.env.VITE_API_URL ||
+    "http://127.0.0.1:8000";
 
 
 // =========================================================
