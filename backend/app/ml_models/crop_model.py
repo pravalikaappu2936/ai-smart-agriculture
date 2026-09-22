146d55976d5f23b60ae08e2400d566522c77da74
Pravalika Appu <pravalikaappu2936@gmail.com>
2026-09-22 14:07:37 +0530

    Fix and update crop recommendation model

--- backend/app/ml_models/crop_model.py
+++ backend/app/ml_models/crop_model.py
@@ -180,21 +180,13 @@ def train_crop_model():
     # =====================================================
 
     model = RandomForestClassifier(
-
-        n_estimators=75,
-
-        max_depth=20,
-
-        min_samples_split=2,
-
-        min_samples_leaf=1,
-
-        random_state=42,
-
-        n_jobs=-1,
-
-        class_weight="balanced"
-
+    n_estimators=40,
+    max_depth=18,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    random_state=42,
+    n_jobs=1,
+    class_weight="balanced",
     )
 
     # =====================================================
--- backend/app/ml_models/train_crop_model.py
+++ backend/app/ml_models/train_crop_model.py
@@ -233,13 +233,13 @@ def train_model():
     )
 
     model = RandomForestClassifier(
-        n_estimators=100,
-        max_depth=None,
-        min_samples_split=2,
-        min_samples_leaf=1,
-        random_state=42,
-        n_jobs=-1,
-        class_weight="balanced"
+    n_estimators=40,
+    max_depth=18,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    random_state=42,
+    class_weight="balanced",
+    n_jobs=1
     )
 
     model.fit(
